763733753f3fff119b756af01367eb57e2b7833c
Ander Piedra <andergust27@gmail.com>
2026-09-10 11:00:39 -0600

    se agregaron comentarios

--- lab2-grupo5/subir.php
+++ lab2-grupo5/subir.php
@@ -1,17 +1,17 @@
 <?php
-
+// nos dice si el archivo fue subido correctamente y no excede el tamaño máximo permitido
     if (isset($_FILES['archivo']) && $_FILES['archivo']['error'] === UPLOAD_ERR_OK) {
-        
+        // Obtenemos información del archivo subido
         $archivoTmpName = $_FILES['archivo']['tmp_name'];
         $nombreOriginal = $_FILES['archivo']['name'];
         $tamanoArchivo  = $_FILES['archivo']['size'];
-        
+        // Definimos el directorio donde se guardarán los archivos subidos
         $directorioDestino = 'uploads/';
-
+// hace una extensión con el nombre del archivo original y le asigna un nombre único para evitar conflictos
         $extension = pathinfo($nombreOriginal, PATHINFO_EXTENSION);
         $nuevoNombre = uniqid('file_', true) . '.' . $extension;
         $rutaFinal = $directorioDestino . $nuevoNombre;
-
+// Verificamos si el tamaño del archivo es menor o igual a 25 MB (25000000 bytes)
         if (move_uploaded_file($archivoTmpName, $rutaFinal)) {
             echo "
             <!DOCTYPE html>
